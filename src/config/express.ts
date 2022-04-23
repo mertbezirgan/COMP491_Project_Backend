8f265cc328f74da6bdc27ef4ac6c92b1e03e8548
Mehmet Mert Bezirgan <mbezirgan18@ku.edu.tr>
2022-04-23 19:40:07 +0300

    enable cors

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,26 +1,28 @@
-import * as bodyParser from 'body-parser';
-import express from 'express';
-const morgan = require('morgan');
+import * as bodyParser from "body-parser";
+import express from "express";
+const morgan = require("morgan");
 
-import authenticate from '../middlewares/authenticate';
-import application from '../constants/application';
-import indexRoute from '../routes/index.route';
-import joiErrorHandler from '../middlewares/joiErrorHandler';
-import * as errorHandler from '../middlewares/apiErrorHandler';
+import authenticate from "../middlewares/authenticate";
+import application from "../constants/application";
+import indexRoute from "../routes/index.route";
+import joiErrorHandler from "../middlewares/joiErrorHandler";
+import * as errorHandler from "../middlewares/apiErrorHandler";
+var cors = require("cors");
 
 const app = express();
+app.use(cors());
 
-require('dotenv').config();
+require("dotenv").config();
 app.use(bodyParser.json());
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(authenticate);
 
 // Router
 app.use(application.url.base, indexRoute);
 app.get("/", (req, res) => {
-    res.send(true);
+  res.send(true);
 });
 
 // Joi Error Handler
